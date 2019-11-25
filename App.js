548d2cce5bc38f78f65e45936c17c35b7276056b
Bilge Mutlu <bmutlu@wisc.edu>
2019-11-25 12:46:22 -0600

    Class demo initial commit

---
+++ App.js
@@ -0,0 +1,122 @@
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Accelerometer } from 'expo-sensors';
+
+const Value = ({name, value}) => (
+  <View style={styles.valueContainer}>
+    <Text style={styles.valueName}>{name}:</Text>
+    <Text style={styles.valueValue}>{new String(value).substr(0, 8)}</Text>
+  </View>
+)
+
+export default class AccelerometerSensor extends React.Component {
+  state = {
+    accelerometerData: {},
+    buttonState: "Stop"
+  };
+
+  componentDidMount() {
+    this._toggle();
+    Accelerometer.setUpdateInterval(250);
+  }
+
+  componentWillUnmount() {
+    this._unsubscribe();
+  }
+
+  _toggle = () => {
+    if (this._subscription) {
+      this._unsubscribe();
+      this.setState({buttonState: "Start"});
+    } else {
+      this._subscribe();
+      this.setState({buttonState: "Stop"});
+    }
+  };
+
+  _subscribe = () => {
+    this._subscription = Accelerometer.addListener(accelerometerData => {
+      this.setState({ accelerometerData });
+    });
+  };
+
+  _unsubscribe = () => {
+    this._subscription && this._subscription.remove();
+    this._subscription = null;
+  };
+
+  render() {
+    let { x, y, z } = this.state.accelerometerData;
+    return (
+      <View style={styles.container}>
+        <Text style={styles.headline}>Accelerometer</Text>
+        <Value name="x" value={x} />
+        <Value name="y" value={y} />
+        <Value name="z" value={z} />
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity onPress={this._toggle} style={styles.button}>
+            <Text style={styles.buttonText}>{this.state.buttonState}</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+}
+
+function round(n) {
+  if (!n) {
+    return 0;
+  }
+
+  return Math.floor(n * 100) / 100;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  headline: {
+    fontSize: 32,
+    textAlign: 'center',
+    margin: 10,
+  },
+  valueContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  valueValue: {
+    width: 100,
+    fontSize: 20
+  },
+  valueName: {
+    width: 50,
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    alignItems: 'stretch',
+    marginTop: 15,
+  },
+  button: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#d0d5d6',
+    padding: 10,
+    marginLeft: 100,
+    marginRight: 100,
+    borderRadius:10,
+  },
+  buttonText: {
+    fontSize: 20,
+  },
+});
